3aa894161daa8763202d806a6ff67e9eab8d6d73
Perseverance <innoceentiaperseverance@gmail.com>
2025-09-13 21:16:08 +0200

    Add script 8-square.js to print a square of Xs based on argument

---
+++ 8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const size = parseInt(process.argv[2], 10);
+
+if (isNaN(size) || size < 1) {
+  console.log("Missing size");
+} else {
+  let i = 0;
+  while (i < size) {
+    console.log("X".repeat(size));
+    i++;
+  }
+}
